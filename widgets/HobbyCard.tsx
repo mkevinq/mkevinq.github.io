0ea51e118df94d202e5cbb2560e66567c140374d
mkevinq <33074023+mkevinq@users.noreply.github.com>
2021-11-25 18:20:22 -0500

    fix hobby card svgs

--- widgets/HobbyCard.tsx
+++ widgets/HobbyCard.tsx
@@ -24,11 +24,12 @@ const HobbyTile: FunctionComponent<Hobby> = ({ title, content, image }) => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
+              (
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 9l-7 7-7-7"
+                strokeWidth={2}
+                d="M5 15l7-7 7 7"
               />
             </svg>
           ) : (
@@ -39,12 +40,11 @@ const HobbyTile: FunctionComponent<Hobby> = ({ title, content, image }) => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              (
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}
-                d="M5 15l7-7 7 7"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
               />
             </svg>
           )}
